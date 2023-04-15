8127ca9552a2c76ca795c3dbf3622da6ca2a6eb2
phudinh153 <phuxa153@gmail.com>
2023-04-15 23:07:35 +0700

    add my profile

--- inc/topBarNav.php
+++ inc/topBarNav.php
@@ -19,7 +19,7 @@ if (isset($_SESSION['user_id']) && $_SESSION['user_id'] > 0) {
 ?>
 <header class="bg-light">
     <nav class="navbar navbar-expand-lg navbar-light bg-light" id="header-nav" role="navigation">
-        <div class="container"><a class="link-dark navbar-brand site-title mb-0" href="#">LMEO SEEKERS</a>
+        <div class="container"><a class="link-dark navbar-brand site-title mb-0" href="#">Worst CV</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
--- profile.php
+++ profile.php
@@ -10,52 +10,50 @@
             <div class="section pt-4 px-3 px-lg-4" id="about">
                 <div class="container-narrow">
                     <div class="row">
+                        <h1 class="text-center">My Profile</h1>
                         <div class="col-md-6">
-                            <h2 class="h4 my-2">Hello! I’m Mai Huu Nghia.</h2>
-                            <p>I am a flexible individual with a great cooperative will. Working under pressure and the
-                                ability to
-                                finish the work as soon as possible are some of my strengths.</p>
-                            <div class="row mt-3">
-                                <div class="col-sm-2">
-                                    <div class="pb-1">Age:</div>
+                            <form action="/attraction" method="post" novalidate class="validated-form">
+                                <div class="mb-3">
+                                <label for="title" class="form-label">First Name<span class='text-danger'>(*)</span>:</label>
+                                <input type="text" class="form-control" name="area[title]" id="title" placeholder="Ex: Smai" required>
                                 </div>
-                                <div class="col-sm-10">
-                                    <div class="pb-1 fw-bolder">21</div>
+                                
+                                <div class="mb-3">
+                                <label for="title" class="form-label">Last Name<span class='text-danger'>(*)</span>:</label>
+                                <input type="text" class="form-control" name="area[title]" id="title" placeholder="Ex: Nghia" required>
                                 </div>
-                                <div class="col-sm-2">
-                                    <div class="pb-1">username:</div>
+
+                                <div class="mb-3">
+                                <label for="location" class="form-label">Email<span class='text-danger'>(*)</span>:</label>
+                                <input type="text" class="form-control" name="area[location]" id="location" placeholder="Ex: nghia@gmail.com" required>
                                 </div>
-                                <div class="col-sm-10">
-                                    <div class="pb-1 fw-bolder">emches1976@gmail.com</div>
+                                <div class="mb-3">
+                                    <label for="image" class="form-label">Phone Number<span class='text-danger'>(*)</span>:</label>
+                                    <input type="text" class="form-control" name="area[image]" id="image" required>
                                 </div>
-                                <div class="col-sm-2">
-                                    <div class="pb-1">Phone:</div>
+                                <div class="mb-3">
+                                    <label for="image" class="form-label">Address:</label>
+                                    <input type="text" class="form-control" name="area[image]" id="image" required>
                                 </div>
-                                <div class="col-sm-10">
-                                    <div class="pb-1 fw-bolder">+8494-226-2713</div>
+                                
+                                <div class="mb-3">
+                                    <button class="btn btn-success">Save</button>
                                 </div>
-                                <div class="col-sm-2">
-                                    <div class="pb-1">Address:</div>
-                                </div>
-                                <div class="col-sm-10">
-                                    <div class="pb-1 fw-bolder">Ho Chi Minh City, Vietnam</div>
-                                </div>
-                                <div class="col-sm-2">
-                                    <div class="pb-1">Staus:</div>
-                                </div>
-                                <div class="col-sm-10">
-                                    <div class="pb-1 fw-bolder">Looking for internship</div>
-                                </div>
-                            </div>
+                            </form>                            
+                         
                         </div>
-                        <div class="col-md-5 offset-md-1" data-aos="fade-left" data-aos-delay="100"><img
-                                class="avatar img-fluid mt-2" src="images/avatar.jpg" width="300" height="300"
-                                alt="Walter Patterson" />
+                        <div class="col-md-5 offset-md-1" data-aos="fade-left" data-aos-delay="100">
+                            <img class="avatar img-fluid mt-2" src="images/avatar.jpg" width="200" height="200" alt="Walter Patterson" />
+
+
+                        
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        
+    </div>
         <?php require_once('inc/footer.php') ?>
         <div id="scrolltop"><a class="btn btn-secondary" href="#top"><span class="icon"><i
                         class="fas fa-angle-up fa-x"></i></span></a></div>
